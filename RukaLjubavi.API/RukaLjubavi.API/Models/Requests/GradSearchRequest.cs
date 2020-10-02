6e7ddbac2dc91927b1e70deff362621a218cf155
Ajdin Ćatić <ajdin.catic@edu.fit.ba>
2020-10-02 15:31:51 +0200

    .

---
+++ RukaLjubavi.API/RukaLjubavi.API/Models/Requests/GradSearchRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RukaLjubavi.API.Models.Requests
+{
+    public class GradSearchRequest
+    {
+    }
+}
