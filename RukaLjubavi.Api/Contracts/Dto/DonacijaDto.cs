ef2da0d7f38fa7dfec6312c26439bb1791c6c0f0
Ajdin Ćatić <ajdin.catic@edu.fit.ba>
2020-10-26 19:16:51 +0100

    .

--- RukaLjubavi.Api/Contracts/Dto/DonacijaDto.cs
+++ RukaLjubavi.Api/Contracts/Dto/DonacijaDto.cs
@@ -13,13 +13,13 @@ namespace RukaLjubavi.Api.Contracts.Dto
         public int Kolicina { get; set; }
         public bool IsPrihvacena { get; set; }
         public DateTime DatumVrijeme { get; set; }
-        public string DonatorId { get; set; }
+        public int DonatorId { get; set; }
         public string DonatorIme { get; set; }
         public string DonatorPrezime { get; set; }
         public string DonatorJmbg { get; set; }
         public DateTime DonatorDatumRodjenja { get; set; }
         public string DonatorMjestoRodjenja { get; set; }
-        public string BenefiktorId { get; set; }
+        public int BenefiktorId { get; set; }
         public string BenefiktorNazivKompanije { get; set; }
         public string BenefiktorPdvbroj { get; set; }
         public string NazivKategorije { get; set; }
